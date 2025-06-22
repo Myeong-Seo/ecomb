73c43f8fbdcf935a0c1ad5517e7c93751ac9eaf9
Myeong <sowkosowko@naver.com>
2025-06-23 05:30:33 +0900

    Test code
    not use DB -> @SpringbootTest don't text on the class name

--- src/main/java/com/example/ecomb/EcombApplication.java
+++ src/main/java/com/example/ecomb/EcombApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcombApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcombApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EcombApplication.class, args);
+    }
 
 }
---
+++ src/test/java/com/example/ecomb/EasyTest.java
@@ -0,0 +1,12 @@
+package com.example.ecomb;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class EasyTest {
+
+    @org.junit.jupiter.api.Test
+     void asd(){
+        System.out.println(1);
+    }
+}
